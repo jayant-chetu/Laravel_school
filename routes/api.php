67239ac7efdf2313f1de40cf059bd9dae50987b5
Jayant Singh <jayantsingh924@gmaile.com>
2022-05-18 19:53:53 +0530

    testing branch commit

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
